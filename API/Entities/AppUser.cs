e8a99dc6db1a9095c29e194d4bb194371dc09c38
Bennet <bexoda@hotmail.com>
2021-06-06 08:21:44 +0000

    created the register account api

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -4,7 +4,7 @@ namespace API.Entities
     {
         public int Id { get; set; }
         public string Username { get; set; }
-        // public string Firstname { get; set; }
-        // public string Surname { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
     }
 }
